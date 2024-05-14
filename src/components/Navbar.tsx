49fdc41832d9d792f56ef94f47ca3842ddde4090
Raț Ioan-Paul <y.ratioanpaul@yahoo.com>
2024-05-14 09:53:32 +0300

    Prima zi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import './Navbar.css';
-import profileImage from '../assets/profile.png'; 
+import { FaTachometerAlt, FaBuilding, FaUsers, FaBox, FaDollarSign, FaCog, FaChevronDown } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="profile-card" onClick={() => window.location.href = '#profile'}>
-        <img src={profileImage} alt="Profile" className="profile-image" />
+      <div className="profile-card">
+        <img className="profile-image" src="profile-image.jpg" alt="Profile" />
         <div className="profile-info">
-          <span className="profile-name">Hana, CEO</span>
+          <div className="profile-name">Hana</div>
+          <div className="profile-title">CEO</div>
+        </div>
+        <FaChevronDown className="profile-arrow" />
+      </div>
+      <div className="admins-menu">
+        <div className="admins-menu-title">
+          <span>Admins menu</span>
         </div>
       </div>
       <div className="navbar-links">
         <ul>
-          <li className="active"><a href="#dashboard">Dashboard</a></li>
-          <li><a href="#business-info">Business Info</a></li>
-          <li><a href="#clients">Clients</a></li>
-          <li><a href="#products">Products</a></li>
-          <li><a href="#balance">Balance</a></li>
+          <li className="active"><a href="#"><FaTachometerAlt className="icon" /> Dashboard</a></li>
+          <li><a href="#"><FaBuilding className="icon" /> Business Info</a></li>
+          <li><a href="#"><FaUsers className="icon" /> Clients</a></li>
+          <li><a href="#"><FaBox className="icon" /> Products</a></li>
+          <li><a href="#"><FaDollarSign className="icon" /> Balance</a></li>
         </ul>
       </div>
-      <div className="navbar-links others">
+      <div className="others">
+        <div className="others-title">
+          <span>Others</span>
+        </div>
         <ul>
-          <li><a href="#settings">Settings</a></li>
+          <li><a href="#"><FaCog className="icon" /> Setting</a></li>
         </ul>
       </div>
     </div>
